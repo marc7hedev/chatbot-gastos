0d2f16719dce015442003c674af337b8847b90c5
marc7hedev <driberstagg@gmail.com>
2024-10-04 16:02:09 -0600

    Creada función appendToSheet

--- utils.js
+++ utils.js
@@ -7,3 +7,26 @@ const auth = new google.auth.GoogleAuth({
 });
 
 const spreadsheetId = '1HIE-IsaXirsBf5-YepSRHQsLTGlKJNMJHH5Yame3g6c';
+
+
+async function appendToSheet(values){
+    const sheets = google.sheets({version: 'v4', auth});
+    const range = 'Sheet1!A1';
+    const valueInputOption = 'USER_ENTERED';
+
+    const resource = { values: values };
+
+    try {
+        const res = await sheets.spreadsheets.values.append({
+            spreadsheetId,
+            range,
+            valueInputOption,
+            resource,
+        })
+        return res;
+    } catch (error) {
+        console.error('error', error);
+    }
+}
+
+module.exports = { appendToSheet };
